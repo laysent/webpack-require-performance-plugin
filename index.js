0530efd4e640c1dc2fbd81e210be47c408af456b
LaySent <laysent@gmail.com>
2019-10-19 23:03:28 +0800

    feat: add Webpack 5 support

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 const defaultOptions = {
   disable: false,
   clearMeasures: false
@@ -7,44 +9,56 @@ function WebpackRequirePerformancePlugin(options) {
   this.options = Object.assign(defaultOptions, options);
 }
 
+function setRequireHook(compiler, callback) {
+  if (compiler.hooks) {
+    compiler.hooks.compilation.tap('WebpackRequirePerformancePlugin', function (compilation) {
+      if (webpack.JavascriptModulesPlugin) {
+        // Webpack 5
+        webpack.JavascriptModulesPlugin.getCompilationHooks(compilation)
+          .renderRequire.tap('WebpackRequirePerformancePlugin', callback);
+      } else {
+        const { mainTemplate } = compilation;
+        mainTemplate.hooks.require.tap('WebpackRequirePerformancePlugin', callback);
+      }
+    });
+    return;
+  }
+  // Deprecated API of Tapable
+  compiler.plugin('compilation', function (compilation) {
+    const { mainTemplate } = compilation;
+    mainTemplate.plugin('require', callback);
+  });
+}
+
 WebpackRequirePerformancePlugin.prototype.apply = function (compiler) {
   const { disable, clearMeasures } = this.options;
   if (disable) return;
-  (compiler.hooks ?
-    compiler.hooks.compilation.tap.bind(compiler.hooks.compilation, 'WebpackRequirePerformancePlugin') :
-    compiler.plugin.bind(compiler, 'compilation')
-  )(function (compilation) {
-    const { mainTemplate } = compilation;
-    (mainTemplate.hooks ?
-      mainTemplate.hooks.require.tap.bind(mainTemplate.hooks.require, 'WebpackRequirePerformancePlugin') :
-      mainTemplate.require.bind(mainTemplate, 'require')
-    )(function (source/*, chunk, hash */) {
-      const beforeExecuteModule = '// Execute the module function';
-      const afterExecuteModule = '// Flag the module as loaded';
-      return source
-        .replace(
+  setRequireHook(compiler, function (source/*, chunk, hash */) {
+    const beforeExecuteModule = '// Execute the module function';
+    const afterExecuteModule = '// Flag the module as loaded';
+    return source
+      .replace(
+        beforeExecuteModule,
+        [
+          '// Begin mark of performance',
+          'if (typeof performance !== "undefined") performance.mark(moduleId);',
+          '',
           beforeExecuteModule,
-          [
-            '// Begin mark of performance',
-            'if (typeof performance !== "undefined") performance.mark(moduleId);',
-            '',
-            beforeExecuteModule,
-          ].join('\n'),
-        )
-        .replace(
+        ].join('\n'),
+      )
+      .replace(
+        afterExecuteModule,
+        [
+          '// End mark of performance',
+          'if (typeof performance !== "undefined") {',
+          '  performance.measure(moduleId, moduleId);',
+          '  performance.clearMarks(moduleId);',
+          clearMeasures ? '  performance.clearMeasures(moduleId);' : '',
+          '}',
+          '',
           afterExecuteModule,
-          [
-            '// End mark of performance',
-            'if (typeof performance !== "undefined") {',
-            '  performance.measure(moduleId, moduleId);',
-            '  performance.clearMarks(moduleId);',
-            clearMeasures ? '  performance.clearMeasures(moduleId);' : '',
-            '}',
-            '',
-            afterExecuteModule,
-          ].join('\n'),
-        );
-    });
+        ].join('\n'),
+      );
   });
 };
 
